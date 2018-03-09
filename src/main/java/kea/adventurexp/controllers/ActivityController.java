4ce00d6e7fa6bceabbd3ae76e7bc91bc4104dff5
JakobBB <jakob.blinkilde@gmail.com>
2018-03-09 09:47:13 +0100

    Update View activities
    Back- and frontend updated.

--- src/main/java/kea/adventurexp/controllers/ActivityController.java
+++ src/main/java/kea/adventurexp/controllers/ActivityController.java
@@ -6,6 +6,7 @@ import kea.adventurexp.services.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,7 +24,14 @@ public class ActivityController {
     @RequestMapping(value = "/viewActivities", method = RequestMethod.GET)
     public String showAllActivites(Model model){
         model.addAttribute("allActivities", activityService.getAllActivities());
-//        activityService.getAllActivities();
+        model.addAttribute("theActivity", activityService.getAllActivities().get(0));
+        return "viewActivities";
+    }
+    @RequestMapping(value = "/viewActivities/{Name}", method = RequestMethod.GET)
+    public String showAllActivites(@PathVariable String Name, Model model){
+        model.addAttribute("allActivities", activityService.getAllActivities());
+        Activity theActivity = activityService.getAnActivity(Name);
+        model.addAttribute("theActivity", theActivity);
         return "viewActivities";
     }
 }
